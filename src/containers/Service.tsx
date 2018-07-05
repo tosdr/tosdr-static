93a19af48faa9651802b29304cafed01bd16f40b
Vincent <projects@vinnl.nl>
2018-07-05 15:46:51 +0200

    Display the title of a quote's case

--- src/containers/Service.tsx
+++ src/containers/Service.tsx
@@ -31,9 +31,9 @@ const renderDocs = (docs: Documents, domain: string) => {
       <section key={slug(filename)} id={slug(filename)} className="section">
         <div className="container">
           <h3 className="title">{filename}</h3>
-          <ul>{docs[filename].points.map(renderQuote)}</ul>
+          <ul className="is-marginless">{docs[filename].points.map(renderQuote)}</ul>
           <hr/>
-          <p>
+          <small>
             <a
               href={`https://github.com/tosdr/tosback2/blob/${docs[filename].topRev}/crawl_reviewed/${domain}/${encodeURIComponent(filename)}`}
               title="View these quotes in their full context on ToSback"
@@ -41,7 +41,7 @@ const renderDocs = (docs: Documents, domain: string) => {
               View the complete {filename} as crawled by ToSback
             </a>&nbsp;
             (<a href={docs[filename].url} title={`View current version of ${filename}`} target="_blank">source</a>)
-          </p>
+          </small>
         </div>
       </section>
     );
@@ -52,24 +52,26 @@ const renderQuote = (quote: Quote, index: number) => {
   return (
     <li key={`quote${index}`} className="media">
       <div className="media-content">
-        <blockquote>{quote.quoteText}</blockquote>
-        <p className="level">
+        <div className="level">
           <div className="level-left">
+            <div className="level-item">
+              <h4 className="title is-size-6">
+                {/* TODO: Refer to the point instead of the case */}
+                <a
+                  href={`https://edit.tosdr.org/cases/${quote.caseId}`}
+                  title="View this point's discussion and history in our rating tool"
+                  target="_blank"
+                >
+                  {quote.caseTitle}
+                </a>
+              </h4>
+            </div>
             <div className="level-item">
               <Classification classification={quote.caseClassification}/>
             </div>
-            <div className="level-item">
-              {/* TODO: Refer to the point instead of the case */}
-              <a
-                href={`https://edit.tosdr.org/cases/${quote.caseId}`}
-                title="View this point's discussion and history in our rating tool"
-                target="_blank"
-              >
-                View discussion
-              </a>
-            </div>
           </div>
-        </p>
+        </div>
+        <blockquote>{quote.quoteText}</blockquote>
       </div>
     </li>
   );

a6c3640f2104e9372cdbeae0b58cac7371fbd908
Vincent <projects@vinnl.nl>
2018-08-29 21:28:02 +0200

    Show rated services first

--- src/components/ServiceList.tsx
+++ src/components/ServiceList.tsx
@@ -9,10 +9,22 @@ interface Props {
 
 export class ServiceList extends React.Component<Props> {
   public render() {
+    const sortedServices = Object.entries(this.props.services)
+      .filter(isCanonicalReview)
+      .sort(([key1, service1], [key2, service2]) => {
+        if (service2.rated === false) {
+          return -1;
+        }
+        if (service1.rated === false) {
+          return 1;
+        }
+
+        return 0;
+      });
+
     return (
       <ul className="is-marginless">
-        {Object.entries(this.props.services)
-          .filter(isCanonicalReview)
+        {sortedServices
           .map(([key, service]: [string, ValidReviewV1]) => (
             <li key={service.slug} className="media">
               <div className="media-content">
